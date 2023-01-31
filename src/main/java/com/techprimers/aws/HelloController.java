d26a44e2aa1d7eb171d4da3979c64acf0edc4479
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2023-01-31 10:08:52 +0530

    test commit for AWS pipeline

--- src/main/java/com/techprimers/aws/HelloController.java
+++ src/main/java/com/techprimers/aws/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping
     public String hello() {
-        return "pipeline chnage on github";
+        return "test commit for AWS pipeline";
     }
 }
