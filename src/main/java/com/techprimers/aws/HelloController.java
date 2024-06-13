de59d402a3d9ae80a4550f2fe31649ac4f1ad4b0
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2024-06-13 10:45:40 +0530

    test commit for code pipeline

--- src/main/java/com/techprimers/aws/HelloController.java
+++ src/main/java/com/techprimers/aws/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping
     public String hello() {
-        return "test change commit on github";
+        return "new code chane by dev on github repo";
     }
 }
